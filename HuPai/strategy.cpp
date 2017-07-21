3157d8b3b35c6612b01c7fca59193dddb1cb9a8b
snowymo <snowymohehe@gmail.com>
2017-07-21 03:14:21 -0400

    strategy value will be updated

---
+++ HuPai/strategy.cpp
@@ -0,0 +1,6 @@
+#include "stragety.h"
+
+Stragety::Stragety()
+{
+
+}
---
+++ HuPai/strategy.h
@@ -0,0 +1,11 @@
+#ifndef STRAGETY_H
+#define STRAGETY_H
+
+
+class Stragety
+{
+public:
+    Stragety();
+};
+
+#endif // STRAGETY_H
